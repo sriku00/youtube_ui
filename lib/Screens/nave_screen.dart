a336b3339b3e9ef8e7f59e142e20b5c861d1c654
Sriku <srikusrikanth2000@gmail.com>
2021-04-13 16:19:19 +0530

    change

--- lib/Screens/nave_screen.dart
+++ lib/Screens/nave_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:miniplayer/miniplayer.dart';
+import 'package:youtube_ui/Models/data.dart';
 import 'package:youtube_ui/Screens/home_screen.dart';
+
+final selectedvideoProvider = StateProvider<Video?>((ref) => null);
 
 class NaveScreen extends StatefulWidget {
   @override
@@ -7,6 +12,7 @@ class NaveScreen extends StatefulWidget {
 }
 
 class _NaveScreenState extends State<NaveScreen> {
+  static const double _miniHeight = 60.0;
   int _selectedIndex = 0;
 
   final _screens = [
@@ -19,17 +25,88 @@ class _NaveScreenState extends State<NaveScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: _screens
-            .asMap()
-            .map((i, screen) => MapEntry(
-                i,
-                Offstage(
-                  offstage: _selectedIndex != i,
-                  child: screen,
-                )))
-            .values
-            .toList(),
+      body: Consumer(
+        builder: (BuildContext context, watch, _) {
+          final selectedVideo = watch(selectedvideoProvider).state;
+
+          return Stack(
+              children: _screens
+                  .asMap()
+                  .map((i, screen) => MapEntry(
+                      i,
+                      Offstage(
+                        offstage: _selectedIndex != i,
+                        child: screen,
+                      )))
+                  .values
+                  .toList()
+                    ..add(
+                      Offstage(
+                        offstage: selectedVideo == null,
+                        child: Miniplayer(
+                          minHeight: _miniHeight,
+                          maxHeight: MediaQuery.of(context).size.height,
+                          builder: (double height, double percentage) {
+                            if (selectedVideo == null) return const SizedBox.shrink();
+
+                            return Container(
+                                color: Theme.of(context).scaffoldBackgroundColor,
+                                child: Column(children: [
+                                  Row(
+                                    children: [
+                                      Image.network(
+                                        selectedVideo.thumbnailUrl,
+                                        fit: BoxFit.cover,
+                                        height: _miniHeight - 4.0,
+                                        width: 120,
+                                      ),
+                                      Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Flexible(
+                                                    child: Text(
+                                                  selectedVideo.title,
+                                                  overflow: TextOverflow.ellipsis,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodyText1!
+                                                      .copyWith(
+                                                          fontWeight: FontWeight.w600,
+                                                          fontSize: 15),
+                                                )),
+                                                Flexible(
+                                                    child: Text(
+                                                  "${selectedVideo.author.username} ",
+                                                  overflow: TextOverflow.ellipsis,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .caption!
+                                                      .copyWith(fontSize: 14),
+                                                ))
+                                              ]),
+                                        ),
+                                      ),
+                                      IconButton(icon: Icon(Icons.play_arrow), onPressed: () {}),
+                                      IconButton(
+                                          icon: Icon(Icons.close),
+                                          onPressed: () {
+                                            context.read(selectedvideoProvider).state = null;
+                                          })
+                                    ],
+                                  ),
+                                  LinearProgressIndicator(
+                                      value: 0.4,
+                                      valueColor: AlwaysStoppedAnimation<Color>(Colors.red)),
+                                ]));
+                          },
+                        ),
+                      ),
+                    ));
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
--- lib/Widgets/video_card_widget.dart
+++ lib/Widgets/video_card_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_ui/Models/data.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:youtube_ui/Screens/nave_screen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class VideoCard extends StatelessWidget {
   final Video video;
@@ -8,32 +11,75 @@ class VideoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Stack(
-        children: [
-          Image.network(
-            video.thumbnailUrl,
-            height: 220,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            right: 8.0,
-            bottom: 8.0,
-            child: Container(
-              padding: EdgeInsets.all(4),
-              color: Colors.black,
-              child: Text(
-                video.duration,
-                style: Theme.of(context).textTheme.caption!.copyWith(color: Colors.white),
-              ),
+    return GestureDetector(
+      onTap: () {
+        context.read(selectedvideoProvider).state = video;
+      },
+      child: Column(children: [
+        Stack(
+          children: [
+            Image.network(
+              video.thumbnailUrl,
+              height: 220,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
-      ),
-      Row(children: [
-        
-      ],)
-    ]);
+            Positioned(
+              right: 8.0,
+              bottom: 8.0,
+              child: Container(
+                padding: EdgeInsets.all(4),
+                color: Colors.black,
+                child: Text(
+                  video.duration,
+                  style: Theme.of(context).textTheme.caption!.copyWith(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                foregroundImage: NetworkImage(video.author.profileImageUrl),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                          child: Text(
+                        video.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(fontWeight: FontWeight.w600, fontSize: 15),
+                      )),
+                      Flexible(
+                          child: Text(
+                        "${video.author.username} * ${video.viewCount}* ${timeago.format(video.timestamp)}",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.caption!.copyWith(fontSize: 14),
+                      ))
+                    ]),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Icon(Icons.more_vert_outlined, size: 20),
+              ),
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
